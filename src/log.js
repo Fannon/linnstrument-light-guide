24c0d819ac61ae61de4b025f4eca091d707e5fc6
Simon Heimler <mail@simon-heimler.de>
2023-04-11 13:56:05 +0200

    Updates

--- src/log.js
+++ src/log.js
@@ -4,21 +4,21 @@ export const log = {
     const logEntry = document.createElement("div");
     logEntry.classList = 'log-entry log-info'
     logEntry.innerHTML = `<small class="text-muted">${getTime()}</small> <span class="msg">${msg}</span>`;
-    document.getElementById("log").appendChild(logEntry);
+    document.getElementById("log").prepend(logEntry);
   },
   warn: (msg) => {
     console.warn(msg)
     const logEntry = document.createElement("div");
     logEntry.classList = 'log-entry log-warn'
     logEntry.innerHTML = `<small class="text-muted">${getTime()}</small> <span class="msg">${msg}</span>`;
-    document.getElementById("log").appendChild(logEntry);
+    document.getElementById("log").prepend(logEntry);
   },
   error: (msg) => {
     console.error(msg)
     logEntry.classList = 'log-entry log-error'
     logEntry.innerHTML = `<small class="text-muted">${getTime()}</small> <span class="msg">${msg}</span>`;
     logEntry.appendChild(textNode);
-    document.getElementById("log").appendChild(logEntry);
+    document.getElementById("log").prepend(logEntry);
   }
 }
 
