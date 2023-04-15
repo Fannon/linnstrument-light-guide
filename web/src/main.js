dbf2740b8bea64f3d305c7304e2be7e4a2636cc3
Simon Heimler <mail@simon-heimler.de>
2023-04-15 18:04:29 +0200

    Using performance.now() instead of Date.now()

--- web/src/main.js
+++ web/src/main.js
@@ -1,21 +1,30 @@
 import { log } from "./log.js";
 import { initConfig, resetConfig, saveConfig, updateSettingsInUI } from "./config.js";
 import { resetGrid, getGridDict, generateGrid, drawGrid } from "./grid.js";
+import { measureNoteTiming, calculateStatistics, logGuideNoteTiming } from "./statistics.js";
+import { createMidiInputRecording, exportMidiInputRecording } from "./recorder.js";
 
 /**
  * Global namespace, aliased to `window.ext`
  */
-const ext = {
+export const ext = {
   config: {},
   history: {
     /** Only played note-on messages */
     playedNotes: [],
-    /** All incoming MIDI input message */
+  },
+  recording: {
+    tick: 0,
+    tickTimer: null,
+    /** instrument incoming MIDI input message */
     midiInput: {
       file: null,
       track: null,
-      tick: 0,
-      tickTimer: null,
+    },
+    /** guide notes incoming MIDI input message */
+    guideInput: {
+      file: null,
+      track: null
     },
   },
   stats: {
@@ -86,6 +95,7 @@ async function registerUiEvents() {
   });
   document.getElementById("reset").addEventListener("click", resetConfig);
   document.getElementById("clear-log").addEventListener("click", clearLog);
+  document.getElementById("clear-history").addEventListener("click", clearHistory);
   document.getElementById("calculate-statistics").addEventListener("click", calculateStatistics);
 
   // UI Resize trigger, with debounce
@@ -116,21 +126,21 @@ async function registerMidiEvents() {
       ext.input.addListener("noteon", (msg) => {
         const noteNumber = msg.dataBytes[0]
         ext.history.playedNotes.push({
-          time: Date.now(),
+          time: performance.now(),
           noteNumber: msg.note.number,
         })
         highlightVisualization(noteNumber, ext.config.playedHighlightColor)
 
         // Add it to MIDI input recording
         const jzzMsg = JZZ.MIDI.noteOn(msg.message.channel, msg.note.number, msg.rawVelocity)
-        ext.history.midiInput.track.add(ext.history.midiInput.tick, jzzMsg);
+        ext.recording.midiInput.track.add(ext.recording.tick, jzzMsg);
       });
       ext.input.addListener("noteoff", (msg) => {
         highlightVisualization(msg.dataBytes[0], 0)
 
         // Add it to MIDI input recording
         const jzzMsg = JZZ.MIDI.noteOff(msg.message.channel, msg.note.number, msg.rawVelocity)
-        ext.history.midiInput.track.add(ext.history.midiInput.tick, jzzMsg);
+        ext.recording.midiInput.track.add(ext.recording.tick, jzzMsg);
       });
       ext.input.addListener("controlchange", (msg) => {
         // Filter out the LinnStrument control (NRPN / RPN) messages
@@ -143,7 +153,7 @@ async function registerMidiEvents() {
         if (!ignoredSubTypes.includes(msg.subtype)) {
           console.log(msg.subtype, msg)
           const jzzMsg = JZZ.MIDI.control(msg.message.channel, msg.controller.number, msg.rawValue)
-          ext.history.midiInput.track.add(ext.history.midiInput.tick, jzzMsg);
+          ext.recording.midiInput.track.add(ext.recording.tick, jzzMsg);
         }
       })
 
@@ -190,11 +200,19 @@ async function registerMidiEvents() {
           const timing = await measureNoteTiming(noteNumber)
           logGuideNoteTiming(timing)
         }
+
+        // Add it to MIDI input recording
+        const jzzMsg = JZZ.MIDI.noteOn(msg.message.channel, msg.note.number, msg.rawVelocity)
+        ext.recording.guideInput.track.add(ext.recording.tick, jzzMsg);
       });
       ext.lightGuideInput.addListener("noteoff", (msg) => {
         const noteNumber = msg.dataBytes[0]
         highlightInstrument(noteNumber, 0)
         highlightVisualization(noteNumber, 0, 'guide')
+
+        // Add it to MIDI input recording
+        const jzzMsg = JZZ.MIDI.noteOff(msg.message.channel, msg.note.number, msg.rawVelocity)
+        ext.recording.guideInput.track.add(ext.recording.tick, jzzMsg);
       });
 
       // Support Synthesia Proprietary 1 (ONE Smart Piano) Light Guide input
@@ -348,6 +366,7 @@ async function getStateFromLinnStrument() {
 }
 
 async function getLinnStrumentParamValue(paramNumber) {
+  const timeout = 250
   return new Promise((resolve, reject) => {
     ext.output.sendNrpnValue(nrpn(299), nrpn(paramNumber), { channels: 1 });
     ext.input.channels[1].addListener("nrpn", (msg) => {
@@ -355,213 +374,28 @@ async function getLinnStrumentParamValue(paramNumber) {
       if (msg.message.dataBytes[0] === 38) {
         return resolve(msg.message.dataBytes[1])
       }
-    }, { duration: 200 })
+    }, { duration: timeout })
     setTimeout(() => {
       reject(new Error(`Timeout when getting NRPN value readout`))
-    }, 100);
+    }, timeout);
   });
 }
 
-/**
- * Measure how the guide note is actually played
- * 
- * TODO: This currently only checks if a played note is close in time to the guide note
- *       There is no detection of wrong played notes that have no guide note, yet
- */
-async function measureNoteTiming(noteNumber) {
-  return new Promise((resolve) => {
-    let pastTimeOffset
-    let futureTimeOffset
-    let foundMatch = false
-    const now = Date.now()
-
-    const result = {
-      noteNumber: noteNumber,
-      noteIdentifier: new Note(noteNumber).identifier,
-      time: now,
-      timingOffset: Infinity,
-    }
-
-    // Detect notes played too early
-    const earlyNote = ext.history.playedNotes.findLast((el) => {
-      return el.noteNumber === noteNumber && el.time >= (now - ext.config.missedNoteThreshold)
-    });
-    if (earlyNote) {
-      pastTimeOffset = earlyNote.time - now
-      if (Math.abs(pastTimeOffset) <= ext.config.delayedNoteThreshold) {
-        // If note is played within `delayedNoteThreshold`, consider it a match right away
-        result.timingOffset = pastTimeOffset
-        return resolve(result)
-      } else if (Math.abs(pastTimeOffset) <= ext.config.missedNoteThreshold) {
-        // If found within `missedNoteThreshold`, mark it as possible match
-        // but keep looking into future for more precise match
-        foundMatch = true
-      }
-    }
-
-    // Detect notes played too late
-    const timeOut = pastTimeOffset ? Math.abs(pastTimeOffset) : ext.config.missedNoteThreshold
-    let poller;
-    poller = setInterval(() => {
-      const lateNote = ext.history.playedNotes.findLast((el) => {
-        return el.time > now && el.noteNumber === noteNumber
-      });
-      if (lateNote) {
-        clearInterval(poller)
-        futureTimeOffset = lateNote.time - now
-        console.log('Found in future', lateNote, futureTimeOffset)
-        foundMatch = true
-
-        if (!pastTimeOffset) {
-          result.timingOffset = futureTimeOffset
-        } else if (Math.abs(futureTimeOffset < Math.abs(pastTimeOffset))) {
-          result.timingOffset = futureTimeOffset
-        } else {
-          result.timingOffset = pastTimeOffset
-        }
-
-        return resolve(result)
-
-      }
-    }, ext.config.delayedNoteThreshold / 4);
-
-    setTimeout((timingOffset) => {
-      clearInterval(poller);
-      if (!foundMatch) {
-        return resolve(result)
-      } else {
-        result.timingOffset = result.timingOffset || pastTimeOffset || 7777
-        return resolve(result)
-      }
-    }, timeOut, result.timingOffset);
-  });
-}
-
-/**
- * Logs and visualizes the Light Guide Note timing statistics
- */
-function logGuideNoteTiming(entry) {
-  ext.stats.guideNoteTimings.push(entry)
-
-  if (Math.abs(entry.timingOffset) > ext.config.missedNoteThreshold) {
-    log.info(`Guide Note ${entry.noteIdentifier} <span class="badge bg-danger">MISSED</span>`)
-  } else if (Math.abs(entry.timingOffset) <= ext.config.delayedNoteThreshold) {
-    if (entry.timingOffset < 0) {
-      log.info(`Guide Note ${entry.noteIdentifier} <span class="badge bg-success">${entry.timingOffset}ms</span>`)
-    } else {
-      log.info(`Guide Note ${entry.noteIdentifier} <span class="badge bg-success">+${entry.timingOffset}ms</span>`)
-    }
-  } else if (entry.timingOffset < 0) {
-    log.info(`Guide Note ${entry.noteIdentifier} <span class="badge bg-info">${entry.timingOffset}ms</span>`)
-  } else {
-    log.info(`Guide Note ${entry.noteIdentifier} <span class="badge bg-primary">+${entry.timingOffset}ms</span>`)
-  }
-
-  const pads = document.getElementsByClassName(`note-number-${entry.noteNumber}`)
-  for (const pad of pads) {
-    if (Math.abs(entry.timingOffset) > ext.config.missedNoteThreshold) {
-      pad.classList.add("played-out-of-time");
-      setTimeout(() => {
-        pad.classList.remove("played-out-of-time");
-      }, ext.config.guideNoteStaticsFadeOut)
-    } else if (Math.abs(entry.timingOffset) <= ext.config.delayedNoteThreshold) {
-      pad.classList.add("played-in-time");
-      setTimeout(() => {
-        pad.classList.remove("played-in-time");
-      }, ext.config.guideNoteStaticsFadeOut)
-    } else if (entry.timingOffset < 0) {
-      pad.classList.add("played-early");
-      setTimeout(() => {
-        pad.classList.remove("played-early");
-      }, ext.config.guideNoteStaticsFadeOut)
-    } else {
-      pad.classList.add("played-late");
-      setTimeout(() => {
-        pad.classList.remove("played-late");
-      }, ext.config.guideNoteStaticsFadeOut)
-    }
-  }
-
-  return entry
-}
 
 function clearLog() {
   document.getElementById("log").innerHTML = ''
-  ext.history.playedNotes = []
-  ext.stats.guideNoteTimings = []
 }
 
-function calculateStatistics() {
-  const stats = {
-    notesPlayed: ext.history.playedNotes.length,
-    guideNotes: ext.stats.guideNoteTimings.length,
-    inTimeNotes: 0,
-    earlyNotes: 0,
-    lateNotes: 0,
-    missedNotes: 0,
-  }
-  let cumulatedTimingOffset = 0
-  let timingOffsetCounter = 0
-  for (const entry of ext.stats.guideNoteTimings) {
-    if (Math.abs(entry.timingOffset) > ext.config.missedNoteThreshold) {
-      stats.missedNotes += 1
-    } else if (Math.abs(entry.timingOffset) <= ext.config.delayedNoteThreshold) {
-      stats.inTimeNotes += 1
-      timingOffsetCounter += 1
-      cumulatedTimingOffset += Math.abs(entry.timingOffset)
-    } else if (entry.timingOffset < 0) {
-      stats.earlyNotes += 1
-      timingOffsetCounter += 1
-      cumulatedTimingOffset += Math.abs(entry.timingOffset)
-    } else {
-      stats.lateNotes += 1
-      timingOffsetCounter += 1
-      cumulatedTimingOffset += Math.abs(entry.timingOffset)
-    }
-  }
-
-  stats.accidentalNotes = Math.max(0, stats.notesPlayed - stats.inTimeNotes - stats.earlyNotes - stats.lateNotes - stats.missedNotes)
-
-  stats.avgTimingOffset = Math.round(cumulatedTimingOffset / (timingOffsetCounter || 1))
-  stats.inTimeNotesRatio = Math.round((stats.inTimeNotes / (stats.notesPlayed || 1)) * 100) / 100
-  stats.earlyNotesRatio = Math.round((stats.earlyNotes / (stats.notesPlayed || 1)) * 100) / 100
-  stats.lateNotesRatio = Math.round((stats.lateNotes / (stats.notesPlayed || 1)) * 100) / 100
-  stats.missedNotesRatio = Math.round((stats.missedNotes / (stats.notesPlayed || 1)) * 100) / 100
-  stats.accidentalNotesRatio = Math.round((stats.accidentalNotes / (stats.notesPlayed || 1)) * 100) / 100
-
-  // Calculate score between 0 and 1000
-  // played early or late notes only give a quarter points
-  // Any differences in played notes and guide notes counts like two missed note
-  stats.score = Math.round((stats.inTimeNotes / (stats.notesPlayed || 1)) * 1000)
-  stats.score += Math.round((stats.earlyNotes / (stats.notesPlayed || 1)) * 250)
-  stats.score += Math.round((stats.lateNotes / (stats.notesPlayed || 1)) * 250)
-  stats.score -= Math.round((stats.accidentalNotes / (stats.notesPlayed || 1)) * 2000)
-  stats.score = Math.max(0, stats.score)
-
-  console.debug(`Aggregated Statistics`, stats)
-
-  let table = `Aggregated Statistics:`
-  table += `<table class="table table-sm">`
-  table += `<thead><tr><th scope="col">Score: ${stats.score}/1000 | Avg. Offset: ${stats.avgTimingOffset}ms</th><th scope="col"># Notes</th><th scope="col">Ratio</th></tr></thead>`
-  table += `<tbody>`
-
-  table += `<tr><th>Notes Played</th><td>${stats.notesPlayed}</td><td></td></tr>`
-  table += `<tr><th class="text-success">In Time Notes</th><td>${stats.inTimeNotes}</td><td>${Math.round(stats.inTimeNotesRatio * 100)}%</td></tr>`
-  table += `<tr><th class="text-info">Early Notes</th><td>${stats.earlyNotes}</td><td>${Math.round(stats.earlyNotesRatio * 100)}%</td></tr>`
-  table += `<tr><th class="text-primary">Late Notes</th><td>${stats.lateNotes}</td><td>${Math.round(stats.lateNotesRatio * 100)}%</td></tr>`
-  table += `<tr><th class="text-warning">Missed Notes</th><td>${stats.missedNotes}</td><td>${Math.round(stats.missedNotesRatio * 100)}%</td></tr>`
-  table += `<tr><th class="text-danger">AccidentalNotes Notes</th><td>${stats.accidentalNotes}</td><td>${Math.round(stats.accidentalNotesRatio * 100)}%</td></tr>`
-
-  table += `</tbody>`
-  table += `</table>`
-
-  log.info(table)
+function clearHistory() {
+  ext.history.playedNotes = []
+  ext.stats.guideNoteTimings = []
+  createMidiInputRecording()
 }
 
 function checkForStatisticsDump() {
   if (ext.stats.guideNoteTimings.length > 0) {
     const lastItem = ext.stats.guideNoteTimings.slice(-1)[0] 
-    if (lastItem.time < Date.now() - ext.config.guideNotesPausedThreshold) {
+    if (lastItem.time < performance.now() - ext.config.guideNotesPausedThreshold) {
       console.debug('Guide Note Timings', ext.stats.guideNoteTimings)
       calculateStatistics()
       ext.stats.guideNoteTimings = []
@@ -571,54 +405,14 @@ function checkForStatisticsDump() {
 
 function checkForMidiDump() {
   if (ext.history.playedNotes.length > 0) {
-    const lastItem = ext.history.playedNotes.slice(-1)[0] 
-    if (lastItem.time < Date.now() - ext.config.playedNotesPausedThreshold) {
+    const lastItem = ext.history.playedNotes.slice(-1)[0]
+    if (lastItem.time < performance.now() - ext.config.playedNotesPausedThreshold) {
       console.debug('Played Note History', ext.history.playedNotes)
       exportMidiInputRecording()
       createMidiInputRecording()
       ext.history.playedNotes = []
     }
   }
-}
-
-function createMidiInputRecording() {
-
-  const tickResolution = 96 // ticks per quarter note
-
-  // https://www.npmjs.com/package/jzz-midi-smf
-  ext.history.midiInput.file = new JZZ.MIDI.SMF(0, tickResolution); // type 0, 96 ticks per quarter note
-  ext.history.midiInput.track = new JZZ.MIDI.SMF.MTrk();
-  ext.history.midiInput.file.push(ext.history.midiInput.track);
-  
-  ext.history.midiInput.track.add(0, JZZ.MIDI.smfSeqName(`Recording: ${new Date().toISOString()}`))
-  ext.history.midiInput.track.add(0, JZZ.MIDI.smfBPM(ext.config.bpm));
-
-  const tickInterval = 60000 / ext.config.bpm / 96
-  ext.history.midiInput.tick = 0;
-  ext.history.midiInput.tick = setInterval(() => {
-    ext.history.midiInput.tick += 1
-  }, tickInterval);
-  
-}
-
-function exportMidiInputRecording() {
-  ext.history.midiInput.track.add(ext.history.midiInput.tick, JZZ.MIDI.smfEndOfTrack());
-
-  const smf = ext.history.midiInput.file
-
-  const fileName = new Date().toISOString().split(':').join('-').split('T').join('_').split('.')[0] + '.mid'
-  // var base64EncodedStr = btoa(unescape(encodeURIComponent(smf.dump())));
-  var base64EncodedStr = btoa(smf.dump());
-
-  const a = document.body.appendChild(
-    document.createElement("a")
-  );
-  a.download = fileName;
-  a.href = "data:audio/midi;base64," + base64EncodedStr;
-  a.innerHTML = fileName;
-
-  log.info('Played Notes MIDI: ' + a.outerHTML)
-
 }
 
 /**
